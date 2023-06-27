99b8be1aad6aa77968e04571e23d35f720aec624
magae1 <38873022+magae1@users.noreply.github.com>
2023-06-27 15:04:23 +0900

    [Bronze V] Title: A+B, Time: 220 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        sum = a+b;
+        System.out.println(sum);
+        
+    }
+}
