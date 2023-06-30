775820b9eb3a04e74f7b6c5c4229162dc228dc8f
magae1 <38873022+magae1@users.noreply.github.com>
2023-06-30 17:13:21 +0900

    [Silver V] Title: 단어 정렬, Time: 92 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,14 +1,4 @@
 import sys
-from functools import cmp_to_key
-
-
-def cmp(x1, x2):
-    if len(x1) == len(x2):
-        for i in range(len(x1)):
-            if x1[i] != x2[i]:
-                return (x1[i] > x2[i]) - (x1[i] < x2[i])
-    return (len(x1) > len(x2)) - (len(x1) < len(x2))
-    
 
 N = int(input())
 words = set()
@@ -16,7 +6,7 @@ for _ in range(N):
     words.add(sys.stdin.readline().rstrip())
 
 words = list(words)
-words.sort(key=cmp_to_key(cmp))
+words.sort(key=lambda x: (len(x), x))
 
 for word in words:
     print(word)
