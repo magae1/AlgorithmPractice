a2cbd3546a7bc7c9ac3fd2e7d536d20581f66962
magae1 <38873022+magae1@users.noreply.github.com>
2024-02-15 00:16:48 +0900

    [Bronze V] Title: 시험 성적, Time: 220 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze V/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+        Scanner ac = new Scanner(System.in);
+		int a = ac.nextInt();
+        if(a>=90){
+            System.out.println("A");
+        }else if(a>=80){
+            System.out.println("B");
+        }else if(a>=70){
+            System.out.println("C");
+        }else if(a>=60){
+            System.out.println("D");
+        }else{
+            System.out.println("F");
+        }
+	}
+}
