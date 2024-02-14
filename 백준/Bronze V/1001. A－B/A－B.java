5fba7476f9545f9f4cbefff0f55cf244860dff6e
magae1 <38873022+magae1@users.noreply.github.com>
2024-02-14 21:16:24 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze V/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b,sub;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        sub = a-b;
+        System.out.println(sub);
+    }
+}
