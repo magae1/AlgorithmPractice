ef67fad4599d44ac5002297a8c8a079acfed166a
magae1 <38873022+magae1@users.noreply.github.com>
2024-02-15 00:06:29 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
