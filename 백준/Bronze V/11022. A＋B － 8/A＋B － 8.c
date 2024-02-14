158fa5ef8be889cc4d669ca3ab7f566a39934161
magae1 <38873022+magae1@users.noreply.github.com>
2024-02-15 00:24:07 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze V/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	int* case_1 = (int*)malloc(sizeof(int) * t);
+	int* case_2 = (int*)malloc(sizeof(int) * t);
+
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d", &case_1[i], &case_2[i]);
+	}
+
+	for (int j = 0; j < t; j++) {
+		printf("Case #%d: %d + %d = %d\n", (j + 1), case_1[j], case_2[j], (case_1[j] + case_2[j]));
+	}
+	return 0;
+}
