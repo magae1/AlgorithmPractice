f4142585e327b9e06d4d49008e54844def6337f8
magae1 <38873022+magae1@users.noreply.github.com>
2024-02-15 00:00:50 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze III/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i > j)
+                cout << ' ';
+            else
+                cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
