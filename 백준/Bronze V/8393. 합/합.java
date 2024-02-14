faa9cfce65303d3cf1cf599f7faaaa81c00cda1c
magae1 <38873022+magae1@users.noreply.github.com>
2024-02-15 00:14:50 +0900

    [Bronze V] Title: 합, Time: 220 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze V/8393. 합/합.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner ac = new Scanner(System.in);
+        int a = ac.nextInt();
+        int sum = 0;
+        for(int i=1;i<=a;i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
