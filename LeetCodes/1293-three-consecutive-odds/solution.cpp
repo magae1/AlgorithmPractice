782716ba96f53f57173f9898cef36d98295590e4
Joongil <jjilil8351@gmail.com>
2024-07-02 13:55:24 +0000

    [Leetcode sync] Runtime - 5 ms (0.42%), Memory - 10.5 MB (100.00%)

---
+++ LeetCodes/1293-three-consecutive-odds/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if (arr.size() < 3) {
+            return false;
+        }
+
+        int count = 0;
+        for (auto iter = arr.begin(); iter != arr.end(); iter++) {
+            if (*iter % 2 == 1) {
+                count += 1;
+                if (count >= 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+};
