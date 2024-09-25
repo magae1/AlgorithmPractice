6ece9af2df86bbe83e3671d65b421d16db87a00f
Joongil <38873022+magae1@users.noreply.github.com>
2024-09-25 14:02:36 +0900

    [Silver III] Title: 주유소, Time: 48 ms, Memory: 2804 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    int min_spot_idx, cur_spot_idx, cur_cost;
+    cin >> N;
+
+    vector<int> distances(N-1, 0);
+    vector<int> costs(N, 0);
+    for (int i = 0; i < N -1; i++) {
+        cin >> distances[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> costs[i];
+    }
+
+    min_spot_idx = 0;
+    cur_cost = 0;
+    for (cur_spot_idx = 0; cur_spot_idx < N - 1; cur_spot_idx++) {
+        if (costs[cur_spot_idx] < costs[min_spot_idx]) {
+            min_spot_idx = cur_spot_idx;
+        }
+        cur_cost += distances[cur_spot_idx] * costs[min_spot_idx];
+    }
+
+    cout << cur_cost << endl;
+    return 0;
+}
