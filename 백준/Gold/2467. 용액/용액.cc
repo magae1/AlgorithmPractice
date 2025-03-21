ddf68ac1ed60ac6421f1e3a0a98ee38183e21369
Joongil <jjilil8351@gmail.com>
2025-03-22 01:39:14 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,43 @@
+#include <climits>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  vector<int> potions(n);
+  for (int i = 0; i < n; i++) {
+    cin >> potions[i];
+  }
+
+  int closest_sum = INT_MAX;
+  int p, q, l, r;
+  p = 0, q = n - 1;
+  l = p, r = q;
+  while (p < q) {
+    int sum = potions[p] + potions[q];
+
+    if (abs(sum) < closest_sum) {
+      l = p, r = q;
+      closest_sum = abs(sum);
+    }
+
+    if (sum < 0) {
+      p += 1;
+    } else if (sum > 0) {
+      q -= 1;
+    } else {
+      break;
+    }
+  }
+
+  cout << potions[l] << ' ' << potions[r] << '\n';
+  return 0;
+}
