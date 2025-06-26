186d3773a48bd424c417d2c96c2cb666f784e699
Joongil <jjilil8351@gmail.com>
2025-06-26 13:21:53 +0900

    [level 2] Title: 올바른 괄호, Time: 5.55 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    private static final char OPEN = '(';
+    private static final char CLOSE = ')';
+    
+    boolean solution(String s) {
+        int stackSize = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == OPEN) {
+                stackSize += 1;
+            } else {
+                if (stackSize <= 0) {
+                    return false;
+                } else {
+                    stackSize -= 1;
+                }
+            }
+        }
+
+        return stackSize > 0 ? false : true;
+    }
+}
