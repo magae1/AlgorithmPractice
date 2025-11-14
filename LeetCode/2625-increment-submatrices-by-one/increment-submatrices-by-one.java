f818eebb340062d16b2b44a1adec94e22afd456e
Joongil <jjilil8351@gmail.com>
2025-11-14 15:22:37 +0900

    Time: 11 ms (78.51%) | Memory: 77.2 MB (18.86%) - LeetSync

--- LeetCode/2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ LeetCode/2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -3,12 +3,29 @@ class Solution {
         int[][] diagram = new int[n][n];
 
         for (int[] query : queries) {
-            for (int i = query[0]; i <= query[2]; i++) {
-                for (int j = query[1]; j <= query[3]; j++) {
-                    diagram[i][j] += 1;
-                }
+            int x1 = query[0], x2 = query[2] + 1;
+            int y1 = query[1], y2 = query[3] + 1;
+            diagram[x1][y1] += 1;
+            if (x2 < n && y2 < n) {
+                diagram[x2][y2] += 1;
+            }
+            if (x2 < n) {
+                diagram[x2][y1] -= 1;
+            }
+            if (y2 < n) {
+                diagram[x1][y2] -= 1;
             }
         }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int above = (i == 0 ? 0 : diagram[i - 1][j]);
+                int left = (j == 0 ? 0 : diagram[i][j - 1]);
+                int prev = (i == 0 || j == 0 ? 0 : diagram[i - 1][j - 1]);
+                diagram[i][j] += above + left - prev;
+            }
+        }
+
         return diagram;
     }
 }
