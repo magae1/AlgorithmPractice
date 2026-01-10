4e6a435e225e569b8363881037b2b2274b45f405
Joongil <jjilil8351@gmail.com>
2026-01-10 11:14:24 +0000

    [Leetcode sync] Runtime - 13 ms (81.02%), Memory - 92.5 MB (5.42%)

--- LeetCodes/2625-increment-submatrices-by-one/solution.java
+++ LeetCodes/2625-increment-submatrices-by-one/solution.java
@@ -1,28 +1,31 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] diagram = new int[n][n];
+        int[][] adding = new int[n][n];
 
         for (int[] query : queries) {
-            int x1 = query[0], x2 = query[2] + 1;
-            int y1 = query[1], y2 = query[3] + 1;
-            diagram[x1][y1] += 1;
-            if (x2 < n && y2 < n) {
-                diagram[x2][y2] += 1;
+            int u = query[1], v = query[0];
+            int x = query[3], y = query[2];
+            adding[v][u] += 1;
+
+            if (x + 1 < n) {
+                adding[v][x + 1] -= 1;
             }
-            if (x2 < n) {
-                diagram[x2][y1] -= 1;
+            if (y + 1 < n) {
+                adding[y + 1][u] -= 1;
             }
-            if (y2 < n) {
-                diagram[x1][y2] -= 1;
+            if (x + 1 < n && y + 1 < n) {
+                adding[y + 1][x + 1] += 1;
             }
+            
         }
 
+        int[][] diagram = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int above = (i == 0 ? 0 : diagram[i - 1][j]);
                 int left = (j == 0 ? 0 : diagram[i][j - 1]);
                 int prev = (i == 0 || j == 0 ? 0 : diagram[i - 1][j - 1]);
-                diagram[i][j] += above + left - prev;
+                diagram[i][j] = adding[i][j] + above + left - prev;
             }
         }
 
