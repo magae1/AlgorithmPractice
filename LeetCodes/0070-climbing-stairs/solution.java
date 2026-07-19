04351f0896a50216f121f1b02e6805049c0ccab4
Joongil <jjilil8351@gmail.com>
2026-07-19 12:10:52 +0000

    [Leetcode sync] Runtime - 0 ms (100.00%), Memory - 42.2 MB (36.26%)

---
+++ LeetCodes/0070-climbing-stairs/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    private int[] dp = new int[46];
+
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        if (dp[n] > 0) {
+            return dp[n];
+        }
+        
+        dp[n] = climbStairs(n - 2) + climbStairs(n - 1);
+        return dp[n];
+    }
+}
